4142d7daff30df147d0d2c9c958d05322d71ad1b
Knifiel <knifiel@gmail.com>
2022-05-24 22:11:11 +0700

    Changes to be committed: 	modified:   script.js
    Adds functions to create grid of any size, populate it with
    children, and add eventListeners to them which call function
    to change children background color to random one.

--- script.js
+++ script.js
@@ -1,17 +1,47 @@
 const container = document.querySelector(".container");
 const gridcontainer = document.createElement("div");
+
 const button = document.createElement('button');
 button.textContent = "Change grid dimensions";
+
 gridcontainer.style.width = "960px";
 gridcontainer.style.height = "960px";
-
 gridcontainer.classList.add('gridContainer');
 gridcontainer.style.display = 'grid';
-for (let i = 0; i<(16**2); i++){
-    const gridElement = document.createElement("div");
-    gridElement.classList.add(`element${i}`);
-    gridcontainer.appendChild(gridElement);
+gridcontainer.style.gap = 0;
+
+container.appendChild(button);
+container.appendChild(gridcontainer);
+
+
+
+function makeGrid(gridsize){
+//calculate size of the resulting element and form a string to pass as parameter to style.gridTemplateX 
+let elementSize = 960/gridsize + "px ";
+elementSize = elementSize.repeat(gridsize);
+elementSize = elementSize.slice(0, elementSize.length - 1);
+
+//pass rows/columns parameters from before
+gridcontainer.style.gridTemplateRows = `${elementSize}`;
+gridcontainer.style.gridTemplateColumns = `${elementSize}`;
+
+//create required number of grid children and put them in documentFragment to append later
+var documentFragment = document.createDocumentFragment();
+for (let i=0; i<(gridsize**2); i++){
+    const element = document.createElement("div");
+    element.className = "element";
+    documentFragment.appendChild(element);
+}
+gridcontainer.appendChild(documentFragment);
+addListeners();
 }
 
-container.appendChild(gridcontainer);
-container.insertBefore(button, gridcontainer);
+function addListeners(){
+document.querySelectorAll(".element").forEach(element => {
+    element.addEventListener("mouseover", event => {
+        var color = `rgb(${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)}`;
+        element.style.backgroundColor = color;
+        console.log(element + "  " + color );
+    })    
+})
+}
